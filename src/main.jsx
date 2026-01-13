6c372520187ae7178d910a2b3f1b2c5c0c369d5a
Alex <alexgllamosas@gmail.com>
2026-01-13 11:45:46 -0300

    feat: proteccion de rutas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRouter";
 import "./index.css";
+import { AuthProvider } from "./context/auth/AuthProvider";
+import { StudentProvider } from "./context/students/StudentProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppRouter />
+    <AuthProvider>
+      <StudentProvider>
+        <AppRouter />
+      </StudentProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from "../pages/auth/Login";
+import Dashboard from "../pages/dashboard/Dashboard";
+import AdminPanel from "../pages/admin/AdminPanel";
+import ProtectedRoute from "./ProtectedRoute";
+import { ROLES } from "../constants/roles";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rutas públicas */}
+        {/* Pública */}
         <Route path="/" element={<Login />} />
+
+        {/* Cualquier logueado */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Solo ADMIN */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={[ROLES.ADMIN]}>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/unauthorized" element={<h1>No autorizado</h1>} />
       </Routes>
     </BrowserRouter>
   );
