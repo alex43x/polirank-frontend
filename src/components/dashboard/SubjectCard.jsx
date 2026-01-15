9bb82c5e8e56326399d610c5fc059a851a1892dc
Alex <alexgllamosas@gmail.com>
2026-01-15 17:10:52 -0300

    fix: cambio de nombre en card de asignaturas

--- src/components/dashboard/SubjectCard.jsx
+++ src/components/dashboard/SubjectCard.jsx
@@ -2,7 +2,7 @@
 export default function SubjectCard({ subject }){
     return (
         <div className="border border-greige text-navy bg-white rounded-lg p-6 h-fit hover:bg-navy hover:text-white transition shadow-md">
-            <span className=" font-medium">{subject.Departmento.nombre}</span>
+            <span className=" font-medium">{subject.Departamento.nombre}</span>
             <h1 className="lg:text-3xl md:text-2xl font-semibold my-2">{subject.nombre}</h1>
         </div>
     )
